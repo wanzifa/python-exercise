77c415657503d45c5fb99740f91eacf49cdd1009
朱承浩 <neo1218@users.noreply.github.com>
2015-08-19 19:05:43 +0800

    添加错误处理

--- 081902.py
+++ 081902.py
@@ -22,6 +22,12 @@ def calculate(a,b,c) :
         return a % c
     elif b == '**' :
         return a ** c
+    elif:
+    # 如果只是做题，可以不加这个处理，但在实际项目中要考虑周全
+    # 对任何用户输入的情况都要考虑到
+    # 而且不要相信任何用户的输入（尤其是在表单中）
+        return "请输入正确的操作符！"
+
 
 x = calculate(a, b, c)
 print '%s = %f' % (expression, x)
